9e8ce9ea4de41e3f95b260d0be3edf2cf6305d93
Andrea Becerra Villamil <andre.bv28gmail.com>
2023-09-05 14:29:19 -0300

    Entrega final

--- main.js
+++ main.js
@@ -1,94 +1,147 @@
 let sum = 0;
 let precioPostal = 0;
-let carrito = [];
+let carrito= [];
+let carritoStorage = [];
+let libroDisponibles = [];
 
-class libros {
-  constructor(id, img, titulo, autor, precio, cantidadCompra = 0) {
-    this.id = id;
-    this.img = img;
-    this.titulo = titulo;
-    this.autor = autor;
-    this.precio = precio;
-    this.cantidadCompra = cantidadCompra;
+/* SE VERIFICA Y SE GUARDA EL CATALOGO EN LOCAL STORAGE */
+async function recuperarlibros() {
+  try {
+    libroDisponibles = JSON.parse(localStorage.getItem("libro"))
+    if(!libroDisponibles) {
+      const response = await fetch("./catalogo.json");
+      const data = await response.json();
+      localStorage.setItem("libro", JSON.stringify(data));
+      libroDisponibles = JSON.parse(localStorage.getItem("libro"))
+    }
+  } catch (error) {
+    document.getElementById("catalogo").innerHTML = `
+      <div class="caja-libro-carrito">
+          <h2>Catalogo no disponible</h2>
+      </div>
+  `;
   }
 }
 
-const libro = [
-  new libros(1, "./img/libro1.jpg", "Escuela de mujeres", "Inma López Silva", 5000),
-  new libros(2, "./img/libro2.jpg", "Madera de cerezo", "Marica Bodrožić", 8000),
-  new libros(3, "./img/libro3.jpg", "QED, las cosas como son", "Gertrude Stein", 6000),
-  new libros(4, "./img/libro4.jpg", "Pombero", "Marina Closs", 7000),
-  new libros(5, "./img/libro5.jpg", "Estado del malestar", "Nina Lykke", 4000),
-  new libros(6, "./img/libro6.jpg", "Cartas a Emma Bowlcut", "Bill Callahan", 1000),
-  new libros(7, "./img/libro7.jpg", "Los años", "Annie Ernaux", 3000),
-  new libros(8, "./img/libro8.jpg", "Una dama perdida", "Willa Cather", 9000),
-  new libros(9, "./img/libro9.jpg", "Las cosas que digo son ciertas", "Blanca Varela", 4500),
-  new libros(10, "./img/libro10.jpg", "Van Gogh el suicidado por la sociedad", "Antonin Artaud", 4000),
-  new libros(11, "./img/libro11.jpg", "El niño que cargaba agua en el colador", "Manoel de Barros", 9000),
-  new libros(12, "./img/libro12.jpg", "La vida secreta de las plantas", "Christopher Bird", 7500),
-];
+/* CUANDO SE CARGA LA PAGINA SE RECUPERA LOS LIBROS DISPONIBLES EN EL CATALOGO */
+document.addEventListener("DOMContentLoaded", async function() {
+    await recuperarlibros();
+    generarCatalogo();
+})
 
-/* CARGA EN EL STORAGE */
-JSON.parse(localStorage.getItem("libro")) || localStorage.setItem("libro", JSON.stringify(libro));
+/* RENDERIZAR EL CATALOGO EN PANTALLA */
+const generarCatalogo = async () => {
+  if(libroDisponibles) {
+    libroDisponibles.forEach((libro) => {
+      let div = document.createElement("div");
+      div.innerHTML = `
+          <div class="caja-libro">
+              <img src="${libro.img}" class="img-catalogo">
+              <h3 id="titulo">${libro.titulo}</h3>
+              <p>${libro.autor}</p>
+              <b>$${libro.precio}</b><br><br>
+              <button id="boton${libro.id}" class="agregar">Agregar al carrito</button>
+          </div>
+      `;
 
-let libroDisponibles = JSON.parse(localStorage.getItem("libro"))
+      document.getElementById("catalogo").append(div);
 
-/* FUNCIÓN PARA RENDERIZAR EL CARRITO */
+    let btnAgregar = document.getElementById(`boton${libro.id}`);
+    btnAgregar.addEventListener("click", () => agregarCarrito(libro.id));
+    });
+  }  
+}
+
+/* RENDERIZAR EL CARRITO */
 const renderizarCarrito = () => {
-    sum = 0;
-    precioPostal = 0;
-    document.getElementById("datosCompra").className = "novisible";
-    document.getElementById("precioFinal").className = "novisible";
-    document.getElementById("finalizar-compra").className = "novisible";
-    document.getElementById("codigo").className = "novisible";
-    document.getElementById("carrito").className = "visible";
+  sessionStorage.setItem("carrito", JSON.stringify(carrito));
+  sum = 0;
+  precioPostal = 0;
+  document.getElementById("datosCompra").className = "novisible";
+  document.getElementById("precioFinal").className = "novisible";
+  document.getElementById("finalizar-compra").className = "novisible";
+  document.getElementById("codigo").className = "novisible";
+  document.getElementById("carrito").className = "visible";
+  carritoStorage =  JSON.parse(sessionStorage.getItem("carrito"));
 
-    let contenedor =  document.getElementById("carrito")
-    contenedor.innerHTML = "";
-    carrito.forEach((item) => {
-      let divCarrito = document.createElement("div");
-      divCarrito.innerHTML = `
-        <div class="caja-libro">
-            <h3 id="titulo">${item.titulo}</h3>
-            <p>${item.autor}</p>
-            <p>Cantidad: ${item.cantidadCompra + 1}</p>
-            <b>$${item.precio}</b><br><br>
-            <button id="btnEliminar${item.id}">Eliminar</button>
-        </div>
-    `;
+  let contenedor =  document.getElementById("carrito")
+  contenedor.innerHTML = "";
+  carritoStorage.forEach((item) => {
+    let divCarrito = document.createElement("div");
+    divCarrito.innerHTML = `
+      <div class="caja-libro-carrito">
+          <h3 id="titulo">${item.titulo}</h3>
+          <p>${item.autor}</p>
+          <p>Cantidad: ${item.cantidadCompra + 1}</p>
+          <b>$${item.precio}</b><br><br>
+          <button id="btnEliminar${item.id}">Eliminar</button>
+      </div>
+  `;
 
     document.getElementById("carrito").append(divCarrito);
     let btnEliminar = document.getElementById(`btnEliminar${item.id}`);
     btnEliminar.addEventListener("click", () => eliminarCarrito(item.id));
-     })
-     carrito.length > 0 ? 
-        (document.getElementById("continuar").className = "visible")
-      : 
-        (document.getElementById("continuar").className = "novisible",     document.getElementById("envio").className = "novisible")
-
-      let continuarEnvio = document.getElementById("continuar");
-      continuarEnvio.addEventListener("click", () => {document.getElementById("envio").className = "visible",
-     document.getElementById("finalizar-compra").className = "visible"
-    })
-
-      if (document.querySelector('input[name="tipo-envio"]')) {
-        
-        document.querySelectorAll('input[name="tipo-envio"]').forEach((elem) => {
-            
-            elem.addEventListener("change", function(event) {
-                let item = event.target.outerHTML;
-                if (item.indexOf("delivery") > -1) {
-
-                document.getElementById("codigo").className = "visible";
-
-            } else {
-                document.getElementById("codigo").className = "novisible";
-            }
-            });
-        });
-      }
+  })
+  carritoStorage.length > 0 ? 
+      (document.getElementById("continuar").className = "visible")
+    : 
+      (document.getElementById("continuar").className = "novisible",     
+      document.getElementById("envio").className = "novisible",
+      sessionStorage.removeItem("carrito"),
+      document.getElementById("compra").className = "popup-close",
+      document.getElementById("cerrar-btn").className = "novisible"
+      )
 }
 
+/* AGREGAR EN CARRITO */
+const agregarCarrito = (id) => {
+
+  let catalogoDisponible = libroDisponibles.find((libro) => libro.id === id);
+  if (!carrito.includes(catalogoDisponible)) { 
+      carrito.push(catalogoDisponible);
+      renderizarCarrito();
+      } else {
+      carrito.forEach((element) => {
+          element === catalogoDisponible && element.cantidadCompra++; 
+      });
+      renderizarCarrito();
+      }
+  document.getElementById("compra").className = "popup";
+  document.getElementById("cerrar-btn").className = "cerrar-btn"
+};
+
+/* ELIMINAR DEL CARRITO */
+const eliminarCarrito = (id) => {
+    let compra = carrito.filter(function(item) {
+        return item.id != id;
+      });
+      carrito = compra;
+      renderizarCarrito();
+}
+
+/* ACCION BOTON CONTINUAR Y SELECCION TIPO DE ENVIO */
+let continuarEnvio = document.getElementById("continuar");
+continuarEnvio.addEventListener("click", () =>  {
+  document.getElementById("envio").className = "visible",
+  document.getElementById("finalizar-compra").className = "visible"
+  if (document.querySelector('input[name="tipo-envio"]')) {
+    document.querySelectorAll('input[name="tipo-envio"]').forEach((elem) => {
+        elem.checked = false;
+        elem.addEventListener("change", function(event) {
+            let item = event.target.outerHTML;
+            if (item.indexOf("delivery") > -1) {
+            document.getElementById("codigo").className = "visible";
+        } else {
+            document.getElementById("codigo").className = "novisible";
+        }
+        });
+    });
+  }
+})
+
+document.getElementById("codigo").addEventListener("input", () => envio(document.getElementById("codigo").value));
+
+/* GUARDA EL VALOR DEL DELIVERY */
 const envio = (codigopostal) => {
     switch(codigopostal.toUpperCase()){
         case 'CABA':
@@ -104,55 +157,103 @@ const envio = (codigopostal) => {
     }
 }
 
-/* FUNCION AGREGAR EN CARRITO */
-const agregarCarrito = (id) => {
 
-    let catalogoDisponible = libroDisponibles.find((libro) => libro.id === id);
-    if (!carrito.includes(catalogoDisponible)) { 
-        carrito.push(catalogoDisponible);
-        renderizarCarrito();
-        } else {
-        carrito.forEach((element) => {
-            element === catalogoDisponible && element.cantidadCompra++; 
-        });
-        renderizarCarrito();}
-     };
-
-/* FUNCION ELIMINAR DEL CARRITO */
-const eliminarCarrito = (id) => {
-    let compra = carrito.filter(function(item) {
-        return item.id != id;
-      });
-      carrito = compra;
-      renderizarCarrito();
-}
-
-/* CARGAR PRODUCTOS EN EL DOM */
-document.addEventListener("DOMContentLoaded", () => {
-    generarCatalogo(libroDisponibles)
-})
-
-const generarCatalogo = (libro) => {
-  libro.forEach((libro) => {
-    let div = document.createElement("div");
-    div.innerHTML = `
-        <div class="caja-libro">
-            <img src="${libro.img}" class="img-catalogo">
-            <h3 id="titulo">${libro.titulo}</h3>
-            <p>${libro.autor}</p>
-            <b>$${libro.precio}</b><br><br>
-            <button id="boton${libro.id}" class="agregar">Agregar al carrito</button>
+/* FUNCION CALCULO TOTAL COMPRA */
+const finalizarCompra = document.getElementById("finalizar-compra");
+carritoStorage =  JSON.parse(sessionStorage.getItem("carrito"));
+finalizarCompra.addEventListener("click", () => {
+    carritoStorage.forEach(({precio, cantidadCompra}) => {
+        sum += precio * (cantidadCompra + 1 );
+        
+    });
+    sum += precioPostal;
+    let subTotal = document.createElement("div");
+    document.getElementById("precioFinal").innerHTML = "";
+    subTotal.innerHTML = `
+        <div>
+            <b>Valor Total: $ ${sum}</b>
         </div>
     `;
 
-    document.getElementById("catalogo").append(div);
-
-  let btnAgregar = document.getElementById(`boton${libro.id}`);
-  btnAgregar.addEventListener("click", () => agregarCarrito(libro.id));
+    document.getElementById("precioFinal").append(subTotal);
+    document.getElementById("precioFinal").className = "visible";
+    document.getElementById("datosCompra").className = "visible";
+    document.getElementById("finalizar-compra").className = "novisible";
+    document.getElementById("envio").className = "novisible";
+    document.getElementById("codigo").className = "novisible";
+    document.getElementById("continuar").className = "novisible";
 });
+
+/* GENERAR ID OPERACION */
+function idOperacion() {
+    return Math.floor(Math.random() * 9999);
 }
 
-/* FUNCION INPUT BUSCADOR */
+/* DATOS DE LA COMPRA */
+const generarCompra = () => {    
+    Swal.fire({
+      title: '¡GRACIAS POR SU COMPRA!',
+      icon: 'success',
+      html:
+        `<h2>Datos Compra</h2><br>` + 
+        `<p>#Operación: ED${idOperacion()}</p><br>` + 
+        `<b>Valor total $ ${sum}</b>`,
+      footer: '<span>Recibira un mail cuando su pedido esté listo.</span>',
+      showCloseButton: true,
+      allowOutsideClick: true,
+      customClass: {
+        closeButton: 'close-sweetalert',
+        confirmButton: 'btn-ok-sweetalert',
+      }
+    })
+
+    document.getElementById("precioFinal").className = "novisible"
+//    document.getElementById("envio").className = "novisible"
+    document.getElementById("carrito").className = "novisible"
+    document.getElementById("carrito").innerHTML = ""
+    //document.getElementById("continuar").className = "novisible"
+    document.getElementById("datosCompra").className = "novisible"
+    //document.getElementById("finalizar-compra").className = "novisible";
+   // document.getElementById("codigo").className = "novisible";
+    document.getElementById("compra").className = "popup-close";
+    sessionStorage.removeItem("carrito");
+    carritoStorage = [];
+    carrito = [];
+}
+
+const datosCompra = document.getElementById("datosCompra");
+datosCompra.addEventListener("click", generarCompra);
+ 
+/* CERRAR CARRITO */
+const btnCerrar = () => {
+    document.getElementById("compra").className = "popup-close";
+}
+
+const cerrarCarrito = document.getElementById("cerrar-btn");
+cerrarCarrito.addEventListener("click", btnCerrar);
+
+/* ABRIR CARRITO */
+const btnCarrito = () => {
+  if (carritoStorage.length > 0) {
+    document.getElementById("compra").className = "popup";
+    document.getElementById("cerrar-btn").className="cerrar-btn";
+  } else {
+    document.getElementById("compra").className = "popup";
+    document.getElementById("cerrar-btn").className="cerrar-btn";
+    document.getElementById("carrito").innerHTML = `
+      <div class="caja-libro-carrito">
+          <h3>No hay libros en el carrito.</h3>
+      </div>
+  `;
+  }
+  
+}
+
+const abrirCarrito = document.getElementById("btn-carrito");
+abrirCarrito.addEventListener("click", btnCarrito);
+
+
+/* BUSCADOR */
 const buscador = () => {
   const cajaBuscador = document.getElementById("buscadorTexto").value.toUpperCase();
   const catalogoLibros = document.getElementById("catalogo");
@@ -162,7 +263,7 @@ const buscador = () => {
   
   mensajeBusqueda.className = "novisible";
 
-  let existencia = libro.find((libro) => libro.titulo.toUpperCase().includes(cajaBuscador));
+  let existencia = libroDisponibles.find((libro) => libro.titulo.toUpperCase().includes(cajaBuscador));
 
   if (existencia) {
     for (let i = 0; i < titulos.length; i++) {
@@ -177,77 +278,12 @@ const buscador = () => {
     }
   } else {
     catalogoLibros.style.display = "";
+    for (let i = 0; i < titulos.length; i++) {
+      contenedorLibro[i].style.display = "none";
+    }
     mensajeBusqueda.className = "visible";
   }
 };
 
 const cajaBuscadorContenedor = document.getElementById("buscadorTexto");
 cajaBuscadorContenedor.addEventListener("input", buscador);
-
-document.getElementById("codigo").addEventListener("input", () => envio(document.getElementById("codigo").value));
-
-/* FUNCION CALCULO TOTAL COMPRA */
-const finalizarCompra = document.getElementById("finalizar-compra");
-finalizarCompra.addEventListener("click", () => {
-    carrito.forEach(({precio, cantidadCompra}) => {
-        sum += precio * (cantidadCompra + 1 );
-        
-    });
-    sum += precioPostal;
-    let subTotal = document.createElement("div");
-    document.getElementById("precioFinal").innerHTML = "";
-    subTotal.innerHTML = `
-        <div>
-            <b>Valor Total: $ ${sum}</b>
-        </div>
-    `;
-
-    document.getElementById("precioFinal").append(subTotal);
-    document.getElementById("precioFinal").className = "visible"
-    document.getElementById("datosCompra").className = "visible"
-});
-
-/* FUNCION GENERAR ID OPERACION */
-function idOperacion() {
-    return Math.floor(Math.random() * 9999);
-}
-
-/* FUNCION DATOS DE LA COMPRA */
-const generarCompra = () => {
-    document.getElementById("contenedorDatosCompra").className = "visible"
-    let datosCompra = document.createElement("div");
-    document.getElementById("ticket-compra") && (document.getElementById("ticket-compra").innerHTML = "")
-    datosCompra.innerHTML = `
-        <div id="ticket-compra">
-            <h2>¡GRACIAS POR SU COMPRA!</h2><br>
-            <h3>Datos Compra</h3><br>
-            <p>#Operación: ED${idOperacion()}</p>
-            <b>Valor total $${sum}</b>
-        </div>
-    `;
-
-    document.getElementById("contenedorDatosCompra").append(datosCompra);
-    document.getElementById("precioFinal").className = "novisible"
-    document.getElementById("envio").className = "novisible"
-    document.getElementById("carrito").className = "novisible"
-    document.getElementById("continuar").className = "novisible"
-    document.getElementById("datosCompra").className = "novisible"
-    document.getElementById("cerrar").className = "visible"
-    document.getElementById("finalizar-compra").className = "novisible";
-    document.getElementById("codigo").className = "novisible";
-}
-
-const datosCompra = document.getElementById("datosCompra");
-datosCompra.addEventListener("click", generarCompra);
-
-const btnCerrar = () => {
-  document.getElementById("contenedorDatosCompra").className = "novisible"
-  document.getElementById("cerrar").className = "novisible"
-  document.getElementById("codigo").className = "novisible";
-  carrito = [];
-  sum = 0;
-  precioPostal = 0;
-}
-
-const cerrarDatos = document.getElementById("cerrar");
-cerrarDatos.addEventListener("click", btnCerrar);
